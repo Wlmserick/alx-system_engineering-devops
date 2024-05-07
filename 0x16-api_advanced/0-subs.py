3d4c45139378a322e4427390d6441372608a6d8c
Wlmserick <erickwlms@gmail.com>
2024-05-07 11:36:57 +0000

    api

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -5,11 +5,11 @@ import requests
 
 def number_of_subscribers(subreddit):
     """Returns the total number of subscribers of subreddit."""
-    subreddit_url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
     headers = {
         "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
     }
-    response = requests.get(subreddit_url, headers=headers, allow_redirects=False)
+    response = requests.get(url, headers=headers, allow_redirects=False)
     if response.status_code == 404:
         return 0
     subs = response.json().get("data")
