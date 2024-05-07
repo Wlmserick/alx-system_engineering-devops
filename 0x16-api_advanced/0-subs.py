1dca643cfdcd4c3c0e76e1f008ca3c483c7b77cf
Wlmserick <erickwlms@gmail.com>
2024-05-07 11:17:20 +0000

    Advanced api

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -8,8 +8,8 @@ def number_of_subscribers(subreddit):
     """Returns the  number of subscribers (not active users,
     total subscribers) for a given subreddit
     """
-    subreddit_url = f"https://www.reddit.com/r/{subreddit}/about.json"
-    headers = {'User-Agent': 'Dry-Lingonberry7149'}
+    subreddit_url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    headers = {'User-Agent': 'linux:0x16.api.advanced:v1.0.0(by/u/bdov_)'}
     try:
         response = requests.get(subreddit_url, headers=headers,
                                 allow_redirects=False).json()
