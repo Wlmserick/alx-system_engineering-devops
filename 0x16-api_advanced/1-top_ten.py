791ee03497f222817273f708dff563edeec9e84f
Erick Wiliams <133129503+Wlmserick@users.noreply.github.com>
2024-05-07 14:40:35 +0300

    Advanced api

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,23 +1,27 @@
 #!/usr/bin/python3
-"""Defines a function that queries a reddit API
+"""
+Function that queries the Reddit API and prints the titles
+of the first 10 hot posts listed for a given subreddit.
 """
 
 import requests
 
 
 def top_ten(subreddit):
-    """Prints the titles of the first 10 hot posts listed for a
-    given subreddit
     """
-    url = f'https://www.reddit.com/r/{subreddit}/hot/.json'
-    payload = {'limit': 10}
-    headers = {'User-Agent': 'Dry-Lingonberry7149'}
+    Function that queries the Reddit API
+    - If not a valid subreddit, print None.
+    """
+    req = requests.get(
+        "https://www.reddit.com/r/{}/hot.json".format(subreddit),
+        headers={"User-Agent": "Custom"},
+        params={"limit": 10},
+    )
 
-    response = requests.get(url, params=payload, headers=headers,
-                            allow_redirects=False).json()
-
-    try:
-        for post in url.get('data').get('children'):
-            print(post.get('data').get('title'))
-    except Exception:
+    if req.status_code == 200:
+        for get_data in req.json().get("data").get("children"):
+            dat = get_data.get("data")
+            title = dat.get("title")
+            print(title)
+    else:
         print(None)
