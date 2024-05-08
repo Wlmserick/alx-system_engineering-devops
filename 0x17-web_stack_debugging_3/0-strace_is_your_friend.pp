076ef6ce7557654c6c760fc2ffc83bb789fbba5e
Erick Wiliams <133129503+Wlmserick@users.noreply.github.com>
2024-05-08 20:03:22 +0300

    Web stack debugging #3

---
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -0,0 +1,6 @@
+# Debug apache 500 error
+exec { 'fix typo' :
+  command => 'sed -i "s/.phpp/.php/g" /var/www/html/wp-settings.php',
+  path    => '/usr/sbin:/usr/bin:/sbin:/bin',
+  onlyif  => 'test -e /var/www/html/wp-settings.php',
+}
